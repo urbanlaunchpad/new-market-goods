439ca5f2a3e2ffb982e538858e44a947aa584e6d
Mehgan Iulo <holler@mehgan.com>
2014-05-18 20:44:36 -0400

    hiding text links from mobile nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,15 @@
 $(document).ready(function(){
 
-//=============HOMEPAGE ANIMATIONS==============
-	//FABRIC02
-	$('#fabric02').hover(function () {
-			// $('#fabric02_text').css({'bottom':'0%', 'padding-top':'.5em'});
-			$('#fabric02_text').slideToggle('slow');
-
-			// console.log('foo');	
-	});
+	//=============MOBILE MENU==============
 
 
 
-//On goods.html (Product page example for now)		
-//Add click event when user selects popover size 
-//until can link with ecommerce backend
+
+	//On goods.html (Product page example for now)		
+	//Add click event when user selects popover size 
+	//until can link with ecommerce backend
 	$('.sizing-chart li').click(function() {
-    $('.sizing-chart li').css({'background-color':'#ffffff', 'color':'#009DDC'});
+	  $('.sizing-chart li').css({'background-color':'#ffffff', 'color':'#009DDC'});
 		$(this).css({'background-color':'#009DDC', 'color':'white'});
 	});
 
