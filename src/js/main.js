951f6c024c667fb3d7e4cb45f73ff2bda30f6fc5
Mehgan Iulo <holler@mehgan.com>
2014-05-19 09:42:27 -0400

    attempts to fix nav bug after clicking hamburger and resizing browser window

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,49 @@
 $(document).ready(function(){
 
 	//=============MOBILE MENU==============
+	var navClone = $('nav').clone();
+
 	$('#mobile-nav').click(function(){
-		// console.log('hi');
+		mobileMenu();
+	});
+
+	function mobileMenu() {
 		$('nav').append($('#nav-left'));
 		$('#nav-right').remove('#nav-right-text');
 		$('#nav-right-text').show();
 		$('#nav-left').removeClass('span_5').addClass('span_12').append($('#nav-right-text')).slideToggle("slow");
-	});
+		
+		//Reset mobile menu on brower window resize
+		//Not quite functional yet
+		$(window).resize(function(navClone){	
+			if ($('#mobile-nav').css('display') == "none" ){
+				$('#nav-left').removeClass('span_12').addClass('span_5').remove('#nav-right-text');
+				$('nav').prepend($('#nav-left'));
+				$('#nav-right').prepend($('#nav-right-text'));
+				// $('nav').replaceWith(navClone.clone());
 
-
-	//Reset mobile menu on brower window resize
-	//NOT WORKING YET!
-	function adaptMenu() {
-		$('.rmm').each(function() {
-			var $width = $(this).css('max-width');
-			$width = $width.replace('px', ''); 
-			if ( $(this).parent().width() < $width*1.05 ) {
-				$(this).children('.rmm-main-list').hide(0);
-				$(this).children('.rmm-toggled').show(0);
 			}
 			else {
-				$(this).children('.rmm-main-list').show(0);
-				$(this).children('.rmm-toggled').hide(0);
+				// $('#nav-left, #nav-right').hide();
 			}
-		});	
+		});
 	}
 
-	$(window).resize(function() {
- 		adaptMenu();
-	});
+
+	// function adaptMenu() {
+	// 	$('.rmm').each(function() {
+	// 		var $width = $(this).css('max-width');
+	// 		$width = $width.replace('px', ''); 
+	// 		if ( $(this).parent().width() < $width*1.05 ) {
+	// 			$(this).children('.rmm-main-list').hide(0);
+	// 			$(this).children('.rmm-toggled').show(0);
+	// 		}
+	// 		else {
+	// 			$(this).children('.rmm-main-list').show(0);
+	// 			$(this).children('.rmm-toggled').hide(0);
+	// 		}
+	// 	});	
+	// }
 
 
 
